950177edc7d833425f89fce170c04a4236626c69
Sh0tlan <x359623@ukr.net>
2023-07-09 23:06:59 +0300

    add utils

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,4 +1,7 @@
 export function formatSecondsToMSS(seconds) {
+  if (!seconds) {
+    return null;
+  }
   return (
     (seconds - (seconds %= 60)) / 60 + (9 < seconds ? ":" : ":0") + seconds
   );
